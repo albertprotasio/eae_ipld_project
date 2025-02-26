d4123863f18930a709d8bb77a379ae3c712b8481
albertprotasio <albertprotasio.es@gmail.com>
2025-02-26 21:13:20 +0100

    Update 0_👋_Home.py

--- 0_👋_Home.py
+++ 0_👋_Home.py
@@ -24,4 +24,46 @@ st.write(f"""<div style="text-align: center;"><h1 style="text-align: center;">�
 
 
 # ----- Profile image file -----
-profile_image_file_path = "profile.…
+profile_image_file_path = "profile.png"       # TODO: Upload your profile image to the same folder as this script and update this if it has a different name
+
+with open(profile_image_file_path, "rb") as img_file:
+    img = "data:image/png;base64," + base64.b64encode(img_file.read()).decode()
+
+
+# ----- Your Profile Image -----
+st.write(f"""
+<div style="display: flex; justify-content: center;">
+    <img src="{img}" alt="Your Name" width="300" height="300" style="border-radius: 50%; object-fit: cover; margin-top: 40px; margin-bottom: 40px;">
+</div>
+""", unsafe_allow_html=True)
+
+
+# ----- Personal title or short description -----
+current_role = "Current Role and/or Studies or Description"   # TODO: Change this
+
+st.write(f"""<div style="text-align: center;"><h4><i>{current_role}</i></h4></div>""", unsafe_allow_html=True)
+
+st.write("##")    # Adding some space
+
+
+# ----- About me section -----
+st.subheader("About Me")
+
+# TODO: Modify and adapt the following lines to your info, you can add or remove some details if you want
+st.write("""
+- 🧑‍💻 I am a <Current Role and/or Studies>
+
+- 🛩️ prev: <Previous Experience, Background or Studies>
+
+- ❤️ <Your Passion and/or Interests>
+
+- 🤖 <Your Personal Projects>
+
+- 🏂 <Your Hobbies>
+
+- 📫 How to reach me: <Your Email>
+
+- 🏠 Barcelona
+""")
+
+# Feel free to add other points like your Linkedin, Github, Social Media, etc.
