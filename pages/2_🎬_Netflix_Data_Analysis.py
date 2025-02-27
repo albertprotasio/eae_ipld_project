e91e68745b486b65adda86117ec04951c1669370
JAP <albertprotasio.es@gmail.com>
2025-02-27 17:11:23 +0100

    updated

--- pages/2_🎬_Netflix_Data_Analysis.py
+++ pages/2_🎬_Netflix_Data_Analysis.py
@@ -46,18 +46,17 @@ with st.expander("Check the complete dataset:"):
 # ----- Extracting some basic information from the dataset -----
 
 # TODO: Ex 2.2: What is the min and max release years?
-min_year = None
-max_year = None
+min_year = movies_df['release_year'].min()
+max_year = movies_df['release_year'].max()
 
 # TODO: Ex 2.3: How many director names are missing values (NaN)?
-num_missing_directors = None
+num_missing_directors = movies_df['director'].isna().sum()
 
 # TODO: Ex 2.4: How many different countries are there in the data?
-n_countries = None
+n_countries = len(all_countries)
 
 # TODO: Ex 2.5: How many characters long are on average the title names?
-avg_title_length = None
-
+avg_title_length = movies_df['title_length'].mean()
 
 # ----- Displaying the extracted information metrics -----
 
@@ -82,7 +81,7 @@ year = cols2[0].number_input("Select a year:", min_year, max_year, 2005)
 
 # TODO: Ex 2.6: For a given year, get the Pandas Series of how many movies and series 
 # combined were made by every country, limit it to the top 10 countries.
-top_10_countries = None
+top_10_countries = df_year['country'].value_counts().head(10)
 
 # print(top_10_countries)
 if top_10_countries is not None:
@@ -102,7 +101,7 @@ st.write("##")
 st.header("Avg Duration of Movies by Year")
 
 # TODO: Ex 2.7: Make a line chart of the average duration of movies (not TV shows) in minutes for every year across all the years. 
-movies_avg_duration_per_year = None
+movies_avg_duration_per_year = movies_df_only.groupby('release_year')['duration'].mean()
 
 if movies_avg_duration_per_year is not None:
     fig = plt.figure(figsize=(9, 6))
